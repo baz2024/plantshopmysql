5a067ba0ab3103d5cfdcd5c6c52742b7ce4a0c7b
Baz2024 <basel.magableh@dbs.ie>
2025-05-15 19:30:11 +0100

    redirect

--- plant-shop-client/src/main.jsx
+++ plant-shop-client/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
